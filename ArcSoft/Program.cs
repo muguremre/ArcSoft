a749b7d40276d2387ba9e7bb521756536c5cfd82
muguremre <muhammeduguremre@gmail.com>
2023-12-12 05:04:09 +0300

    Added Auth

--- ArcSoft/Program.cs
+++ ArcSoft/Program.cs
@@ -2,6 +2,12 @@
 using Business.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.IO;
+using System.Text;
 
 namespace ArcSoft
 {
@@ -10,35 +16,95 @@ namespace ArcSoft
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Configuration.AddJsonFile(Path.Combine(builder.Environment.ContentRootPath, "appsettings.json"));
 
-            // Add services to the container.
+            // JWT Secret Key
+            var jwtKey = builder.Configuration["JWT:SecretKey"];
+            if (string.IsNullOrEmpty(jwtKey))
+            {
+                // Eğer JWT:SecretKey değeri appsettings.json'da bulunmuyorsa, varsayılan bir değer kullanabilirsiniz.
+                jwtKey = "DefaultSecretKey";
+            }
+            var key = Encoding.ASCII.GetBytes(jwtKey);
+
+            // Issuer ve Audience kontrolü
+            var issuer = builder.Configuration["JWT:Issuer"];
+            var audience = builder.Configuration["JWT:Audience"];
+
+            if (string.IsNullOrEmpty(issuer) || string.IsNullOrEmpty(audience))
+            {
+                throw new InvalidOperationException("JWT:Issuer ve JWT:Audience değerleri belirtilmelidir.");
+            }
+
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience
+                };
+            });
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddSingleton<ILoginService, LoginManager>(); // IComputerService istenirse ComputerManager ver
-            builder.Services.AddSingleton<ILoginDal, EfLoginMaterialsDal>(); // IComputerDal istenirse EfComputerDal ver
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+            builder.Services.AddSingleton<ILoginService, LoginManager>();
+            builder.Services.AddSingleton<ILoginDal, EfLoginMaterialsDal>();
+            builder.Services.AddSingleton<IBearerTokenService, BearerTokenService>();
 
             builder.Services.AddCors(options =>
-                    options.AddDefaultPolicy(builder =>
+                options.AddDefaultPolicy(builder =>
                     builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));
 
             var app = builder.Build();
-           app.UseCors();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ArcSoft API V1");
+                });
             }
 
             app.UseHttpsRedirection();
-
+            app.UseCors();
+            app.UseAuthentication(); // JWT Authentication ekledik
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
---
+++ Business/Concrete/BearerTokenService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Business.Abstract;
+
+public class BearerTokenService : IBearerTokenService
+{
+    private readonly IConfiguration _configuration;
+
+    public BearerTokenService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string GenerateToken(Guid LoginMaterialId)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_configuration["Jwt:SecretKey"]);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[] { new Claim("id", LoginMaterialId.ToString()) }), // Guid'i string'e çevir
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
+
+}
